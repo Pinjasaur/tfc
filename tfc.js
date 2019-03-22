5c515733686401292b6f7b54f8605fe530b23f8c
Paul Esch-Laurent <herppfel@gmail.com>
2019-03-22 13:43:46 -0400

    Write out files

--- tfc.js
+++ tfc.js
@@ -1,45 +1,53 @@
 // Standard libraries
 const {
-  readFileSync
+  readFileSync,
+  writeFile
 } = require('fs')
 
 // NPM packages
 const axios = require('axios')
 const parse = require('url-parse')
+const mkdirp = require('mkdirp')
+const rimraf = require('rimraf')
 
 // Utils
+const noop = () => {}
 const trimAndUnique = arr => arr.map(i => i.trim()).filter(i => i !== "").filter((x, i, a) => a.indexOf(x) === i)
-const go = async res => {
+const go = res => {
   stats.total++
-  const url = res.request.res.responseUrl
-
   // Content type a text file?
   if (!res.headers['content-type'].toLowerCase().includes('text/plain')) return
 
   // Probably not an HTML document?
   if (/^<!doctype html/i.test(res.data.trim())) return
 
+  const url  = res.request.res.responseUrl
   const path = parse(url).pathname.toLowerCase()
+  const host = parse(url).hostname.toLowerCase()
+  const file = res.data
 
-  if (path.endsWith('robots.txt'))   stats.robots++
-  if (path.endsWith('humans.txt'))   stats.humans++
-  if (path.endsWith('security.txt')) stats.security++
+  if (path.endsWith('robots.txt'))   { stats.robots++;   write(host, 'robots',   file) }
+  if (path.endsWith('humans.txt'))   { stats.humans++;   write(host, 'humans',   file) }
+  if (path.endsWith('security.txt')) { stats.security++; write(host, 'security', file) }
 
   return res
 }
-const done = () => {
+const done = results => {
   clearInterval(update)
   console.log(`Done (${stats.total} responses).\n`)
   console.log(`robots.txt:\t${(stats.robots / domains.length).toFixed(4) * 100}% (${stats.robots} of ${domains.length})`)
   console.log(`humans.txt:\t${(stats.humans / domains.length).toFixed(4) * 100}% (${stats.humans} of ${domains.length})`)
   console.log(`security.txt:\t${(stats.security / domains.length).toFixed(4) * 100}% (${stats.security} of ${domains.length})`)
 }
-const write = (domain, file, data) =>
+const write = (domain, file, data) => writeFile(`files/${domain}_${file}.txt`, data, (err) => err ? console.log(err) : noop())
 
 process.on('SIGINT', () => {
   done()
   process.exit()
 })
+
+rimraf.sync('files')
+mkdirp.sync('files')
 
 const stats = { total: 0, robots: 0, humans: 0, security: 0 }
 const files = ['/robots.txt', '/humans.txt', '/.well-known/security.txt']
@@ -63,6 +71,7 @@ const opts = {
   headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0' },
   maxRedirects: 20,
   // timeout: 5 * 60 * 1000
+  // timeout: ((domains.length * 3) / 10) * 1000
 }
 
 let update = setInterval(() => console.log(`${stats.total} responses...`), 5 * 1000)
